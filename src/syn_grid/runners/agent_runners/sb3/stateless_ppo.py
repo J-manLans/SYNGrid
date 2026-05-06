Fixed reward scaling.
I used a multiplier in my new threshold scoring type scenario and dwarfed scores by 0.5. This destroyed the exploring signal a little bit so the model didn't converge as fast and with a little less optimal behavior. So I set it to 1.0 as a default and noted that it can be modified if magnitudes in reward gets to big.

It might just add unnecessary computation though. So keep this in mind running experiments later, might remove it if fps takes to much of a bang (every millistep is valuable when we run a 10M training sesch :D).

src/syn_grid/runners/agent_runners/sb3/stateless_ppo.py
@@ -64,11 +64,11 @@ class StatelessPPO(BaseSB3Runner[PPO]):
                     episode_rewards.append(info[0].get("reward"))
                     step_count += 1
 
-                    print(
-                        f'Episode {i}, reward: {episode_rewards[-1]}, droid score: {info[0]["score"]}, sum of rewards: {np.sum(episode_rewards)}'
-                    )
+                    # print(
+                    #     f'Episode {i}, reward: {episode_rewards[-1]}, droid score: {info[0]["score"]}, sum of rewards: {np.sum(episode_rewards)}'
+                    # )
                     if done_arr[0]:
-                        print()
+                        # print()
                         break
 
                 episode_lengths.append(step_count)
@@ -82,7 +82,7 @@ class StatelessPPO(BaseSB3Runner[PPO]):
         avg_length = sum(episode_lengths) / len(episode_lengths)
         sum_rew = sum(r for r in all_rewards)
         avg_rew = sum_rew / self._eval_conf.num_eval_episodes
-        max_tier_reached = sum(1 for r in all_rewards if r == 9)
+        max_tier_reached = sum(1 for r in all_rewards if r == 14)
         avg_max_tier = max_tier_reached / self._eval_conf.num_eval_episodes
         num_tier_out_of_order = sum(1 for r in all_rewards if r == -2)
         average_tier_out_of_order = (